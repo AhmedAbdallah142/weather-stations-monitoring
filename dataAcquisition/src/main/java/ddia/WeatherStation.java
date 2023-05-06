b2737e2333192a34a7a7d225dec7f0ec386b2d42
Ahmed Yusuf <27med99@gmail.com>
2023-05-06 20:02:36 +0300

    Read kafka from environment

--- dataAcquisition/src/main/java/ddia/WeatherStation.java
+++ dataAcquisition/src/main/java/ddia/WeatherStation.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.Optional;
 import java.util.Properties;
 import java.util.Random;
 
@@ -17,10 +18,10 @@ public class WeatherStation {
 
     public static void main(String[] args) throws InterruptedException {
         int STATION_ID = Integer.parseInt(args[0]);
+        String kafka = Optional.ofNullable(System.getenv("kafka")).orElse("localhost:9092");
 
         Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
