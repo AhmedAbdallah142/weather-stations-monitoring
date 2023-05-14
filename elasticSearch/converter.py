c2b2620736a62ed9558665e944dbcb4c9674aa43
Mark Magdy <58529681+mark74384@users.noreply.github.com>
2023-05-15 00:43:47 +0200

    update elastic search files

--- elasticSearch/converter.py
+++ elasticSearch/converter.py
@@ -1,24 +1,43 @@
-# # importing the libraries used
-# import pandas as pd
-#
-# # initializing the data
-# data = {
-#     "station_id": 1,
-#     "s_no": 1,
-#     "battery_status": "low",
-#     "status_timestamp": 1681521224,
-#     "weather": {
-#         "humidity": 35,
-#         "temperature": 100,
-#         "wind_speed": 13
-#     }
-# }
-# df = pd.json_normalize(data)
-# df = pd.json_normalize(data,max_level=0)
-# df.to_parquet("out.parquet")
-from datetime import datetime
+from elasticsearch import Elasticsearch
 
-from datetime import datetime
-current_date = datetime.now()
-print(current_date.strftime('%Y-%m-%dT%H:%M:%S.%f%z'))
+es = Elasticsearch(["http://localhost:9200"])
 
+# Define the Elasticsearch index name
+index_name = "weather-stations4"
+
+# Define the station ID to search for
+station_id = 1
+
+# Define the range of s_no values to check
+start_s_no = 1
+end_s_no = 1000
+
+# Create the Elasticsearch query
+query = {
+    "query": {
+        "bool": {
+            "must": [
+                {"term": {"station_id": station_id}},
+                {"range": {"s_no": {"gte": start_s_no, "lte": end_s_no}}}
+            ]
+        }
+    },
+    "size": 1000
+}
+
+# Execute the query and get the s_no values
+s_no_values = set()
+result = es.search(index=index_name, body=query)
+hits = result["hits"]["hits"]
+for hit in hits:
+    s_no = hit["_source"]["s_no"]
+    s_no_values.add(s_no)
+
+# Find the missing s_no values
+missing_s_no_values = set(range(start_s_no, end_s_no+1)) - s_no_values
+
+# Print the missing s_no values
+if missing_s_no_values:
+    print(f"Missing s_no values for station {station_id}: {missing_s_no_values}")
+else:
+    print(f"All s_no values present for station {station_id}")
--- elasticSearch/demo.py
+++ elasticSearch/demo.py
@@ -6,44 +6,48 @@ import datetime
 import pandas as pd
 
 spark = SparkSession.builder.appName("ParquetToES").getOrCreate()
-parquet_dir = "parquet_files"
+parquet_dir = "C:\\Users\\Kimo Store\\Desktop\\weather-stations-monitoring\\centralStation\\archive"
 timeStamp = 0
 
-for file in os.listdir(parquet_dir):
-    filename = os.path.basename(file)[:-len(".parquet")]
-    file_timestamp = int(filename)
-    if file.endswith(".parquet") and file_timestamp > timeStamp:
-        df = pd.read_parquet(os.path.join(parquet_dir, file))
-        #
-        #
-        #
-        index_name = "weather-stations3"
+# for file in os.listdir(parquet_dir):
+for (dirpath, _, filenames) in os.walk(parquet_dir):
+    for file in filenames:
+        if file.endswith("crc"):
+            continue
+        filename = os.path.basename(file)[:-len(".parquet")]
+        file_timestamp = int(filename)
+        if file.endswith(".parquet"):
+            df = pd.read_parquet(os.path.join(dirpath, file))
+            #
+            #
+            #
+            index_name = "weather-stations4"
 
-        # Define the Elasticsearch connection settings
-        es = Elasticsearch(["http://localhost:9200"])
+            # Define the Elasticsearch connection settings
+            es = Elasticsearch(["http://localhost:9200"])
 
-        for i, row in df.iterrows():
-            humidity = row["weather"]["humidity"]
-            temperature = row["weather"]["temperature"]
-            wind_speed = row["weather"]["windSpeed"]
-            doc = {
-                "station_id": row["stationId"],
-                "s_no": row["sNo"],
-                "battery_status": row["batteryStatus"],
-                "status_timestamp": row["statusTimestamp"],
-                "weather": [{
-                    "humidity": humidity,
-                    "temperature": temperature,
-                    "wind_speed": wind_speed
+            for i, row in df.iterrows():
+                humidity = row["weather"]["humidity"]
+                temperature = row["weather"]["temperature"]
+                wind_speed = row["weather"]["windSpeed"]
+                doc = {
+                    "station_id": row["stationId"],
+                    "s_no": row["sNo"],
+                    "battery_status": row["batteryStatus"],
+                    "status_timestamp": row["statusTimestamp"],
+                    "weather": [{
+                        "humidity": humidity,
+                        "temperature": temperature,
+                        "wind_speed": wind_speed
+                    }
+                    ]
                 }
-                ]
-            }
-            es.index(index=index_name, document=doc)
-            #
-            #
-            #
+                es.index(index=index_name, document=doc)
+                #
+                #
+                #
 
-        timeStamp = file_timestamp  # update timeStamp
-        print("Loaded file with timestamp", timeStamp)
+            timeStamp = file_timestamp  # update timeStamp
+            print("Loaded file with timestamp", timeStamp)
 
 print("Latest timestamp:", timeStamp)
